676cca140dd217160902bb26132feafcb10e18ad
pilarik31 <pilarjan4111@gmail.com>
2018-10-16 11:49:53 +0200

    another script

---
+++ autowget.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+input="wgetlist.txt"
+outputDir="./wgetautoOutput/"
+
+echo "===KONTROLA==="
+echo "Spuštěno z: $PWD"
+echo "Vstup: $input"
+echo "Výstupní složka: $outputDir"
+if [ ! -f $input ] 
+then
+    echo "Vstupní soubor ($input) neexistuje! Ukončuji..."
+    exit
+else
+    echo "Vstupní soubor existuje."
+fi
+
+if [ ! -d $outputDir ] 
+then
+    echo "Výstupní složka neexistuje, vytvářím $outputDir"
+    mkdir  $outputDir
+else
+    echo "Výstupní složka existuje."
+fi
+echo "===KONEC KONTROLY==="
+echo "===ZAČÁTEK STAHOVÁNÍ==="
+
+
+while IFS= read LINE 
+do
+    wget $LINE -P $outputDir
+done <"$input"
+
+
+
